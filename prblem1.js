205c7fcc516941490836c4c234f454e0ddd90030
devasenak166 <devasenakittusamy166@gmail.com>
2023-04-06 09:42:12 +0530

    first commit

---
+++ prblem1.js
@@ -0,0 +1,16 @@
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12 };
+//     document.write(Object.keys(student));
+//     for(const key of Object.keys(student)){
+//         console.log(key);
+//     }
+
+
+
+let colors=["red","orange","blue","green","yellow","purple"]
+console.log(colors[0]);
+console.log(colors[2]);
+colors[length-1]="ultraviolet";
+console.log(colors);
